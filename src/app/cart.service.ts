e3e754a6e0707b6326fbe5b7a41a1bfd3b86fba8
lorayyyn <takingbreathtostay@gmail.com>
2021-10-04 16:43:13 +0200

    1 file added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items: Product[] = [];
+
+  constructor() { }
+
+}
